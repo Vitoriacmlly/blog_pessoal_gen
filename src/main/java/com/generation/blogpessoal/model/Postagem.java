fc32e5c10c6cbff2969063dc0d4be125133d3551
Vitoriacmlly <vcamilly18@gmail.com>
2025-04-16 19:37:26 -0300

    feat: crud

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDate;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -31,6 +34,10 @@ public class Postagem {
 	
 	@UpdateTimestamp 
 	private LocalDate data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem") 
+	private Tema tema; 
 	
 	
 	public Long getId() {
@@ -56,6 +63,12 @@ public class Postagem {
 	}
 	public void setData(LocalDate data) {
 		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
